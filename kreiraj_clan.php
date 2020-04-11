cb387a9e12597e0889872c05cf3ff2644ed22771
itomic-veleri <itomic@veleri.hr>
2020-04-11 23:22:52 +0200

    Update kreiraj_clan.php

--- kreiraj_clan.php
+++ kreiraj_clan.php
@@ -9,25 +9,20 @@
 		echo "Baza podataka nije izabrana";
     }
     
-    $fields = array('ime','prezime','oib'); //kreianje polja
-    
-        //fill polja
-        $ime = $_POST['ime'];
-		$prezime = $_POST['prezime'];
-		$oib = $_POST['oib'];
+    //fill varijable
+    $ime = $_POST['ime'];
+    $prezime = $_POST['prezime'];
+    $oib = $_POST['oib'];
+   
+    //sql upit za punjenje tablice
+    $sql = "INSERT INTO clanovi (ime, prezime, oib) VALUES('$ime','$prezime','$oib')";
 
-        //sql upit za punjenje tablice
-        $sql = "INSERT INTO clanovi (ime,prezime,oib) VALUES('$ime','$prezime','$oib')";
-
-        //Uspješno . neuspješno punjenje
-        if(!mysqli_query($con,$sql)){
-			echo "Podaci nisu uneseni u bazu podataka";
-			
-		}
-		else{
-			header('Location: clanovi.php');
-		}
-		
-		
-	}
+    //Uspješno . neuspješno punjenje
+    if(!mysqli_query($con,$sql)){
+        echo "Podaci nisu uneseni u bazu podataka";
+        
+    }
+    else{
+        header('Location: clanovi.php');
+    }
 ?>
