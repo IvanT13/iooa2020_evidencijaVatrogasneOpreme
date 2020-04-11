6dc46b726a5c7786511d5dc7e30e4c1858dcd2cf
itomic-veleri <itomic@veleri.hr>
2020-04-11 22:21:52 +0200

    Dodan kod za spremanje podataka u baz

--- kreiraj_clan.php
+++ kreiraj_clan.php
@@ -1,3 +1,33 @@
 <?php
+    $con = mysqli_connect('127.0.0.1','root',''); //spajanje na server
 
+    if(!$con){
+		echo "Neuspjelo spajanje na bazu";
+    }
+    
+    if(!mysqli_select_db($con,'iooa2020')){ //spajanje na bazu
+		echo "Baza podataka nije izabrana";
+    }
+    
+    $fields = array('ime','prezime','oib'); //kreianje polja
+    
+        //fill polja
+        $ime = $_POST['ime'];
+		$prezime = $_POST['prezime'];
+		$oib = $_POST['oib'];
+
+        //sql upit za punjenje tablice
+        $sql = "INSERT INTO clanovi (ime,prezime,oib) VALUES('$ime','$prezime','$oib')";
+
+        //Uspješno . neuspješno punjenje
+        if(!mysqli_query($con,$sql)){
+			echo "Podaci nisu uneseni u bazu podataka";
+			
+		}
+		else{
+			header('Location: clanovi.php');
+		}
+		
+		
+	}
 ?>
