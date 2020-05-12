551b51e6ce015fe128fc381ca43c0e5c58398500
itomic-veleri <itomic@veleri.hr>
2020-05-12 23:38:34 +0200

    dodana prd konekcija

--- connection.php
+++ connection.php
@@ -1,4 +1,6 @@
 <?php
+    //test
+    /*
     $con = mysqli_connect("localhost","root",""); //spajanje na server
 
     if(!$con){
@@ -6,6 +8,17 @@
     }
     
     if(!mysqli_select_db($con,'iooa2020')){ //spajanje na bazu
+		echo "Baza podataka nije izabrana";
+    }
+    */
+    //produkcija
+    $con = mysqli_connect("dvdbribir.hr","dvdbribir_ivan","K0mpl1c1r4naL0z!nka"); //spajanje na server
+
+    if(!$con){
+		echo "Neuspjelo spajanje na bazu";
+    }
+    
+    if(!mysqli_select_db($con,'dvdbribir_iooa2020')){ //spajanje na bazu
 		echo "Baza podataka nije izabrana";
     }
 ?>
