90cb360d161f506f5adca79bcaf1466e67a3b0b0
itomic-veleri <itomic@veleri.hr>
2020-05-12 23:46:17 +0200

    prepravljane sql upit

--- azuriraj_zastitna_oprema_back.php
+++ azuriraj_zastitna_oprema_back.php
@@ -6,6 +6,9 @@
     $naziv_opreme = $_POST['naziv_opreme'];
     $kolicina = $_POST['kolicina'];
     $clan = $_POST['clan'];
+    $datum_post = $_POST['datum_zaduzeno'];
+    $datum_convert = strtotime($datum_post);
+    $datum_zaduzeno = date('Y-m-d',$datum_convert);
     $sql = "SELECT * FROM clanovi";
     $myData = mysqli_query($con,$sql); //pull podataka iz baze
     while($record = mysqli_fetch_array($myData)){
@@ -16,7 +19,7 @@
       }   
    
     //sql upit za punjenje tablice
-    $sql = "UPDATE zastitna_oprema SET naziv_opreme = '$naziv_opreme', kolicina = '$kolicina', clan= '$clan' WHERE ID = '$id'";
+    $sql = "UPDATE zastitna_oprema SET naziv_opreme = '$naziv_opreme', kolicina = '$kolicina', clan= '$clan', datum_zaduzeno='$datum_zaduzeno'  WHERE ID = '$id'";
 
     //Uspješno . neuspješno punjenje
     if(!mysqli_query($con,$sql)){
